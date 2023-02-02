a4d5f00fefc52b88978c4cd7f7ee81b3dadada32
Engels Antero <engelsantero@gmail.com>
2023-02-02 14:07:47 -0300

    Add files via upload

---
+++ echo_print.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+
+	<?php
+
+		echo 'Testando o echo <br>';
+
+		print 'Testando o print<hr>';
+
+	 ?>
+</head>
+<body>
+
+</body>
+</html>
